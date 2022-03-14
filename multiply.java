59eff9ce2182ce07073709571dcc95967084814e
Saiyam Gupta <saiyam2809@gmail.com>
2022-03-14 15:28:41 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class Sum{
+    
+    Run | Debug
+    public private static main (String[] args) {
+        int a = 6;
+        int b = 5
+
+        System.out.println("the product is"+(a*b));
+        
+    }
+    }
+
+}
